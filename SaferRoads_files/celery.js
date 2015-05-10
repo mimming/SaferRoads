8a0aefba2fffdf09909354f5503fb351f9d4e471
Jenny Tong <jen@firebase.com>
2015-05-09 19:17:33 -0700

    pulled webpage over

---
+++ SaferRoads_files/celery.js
@@ -0,0 +1,7 @@
+(function() {
+  if (document.isCeleryLoaded) { return; }
+  document.isCeleryLoaded = true;
+  var c = document.createElement('script'); c.type = 'text/javascript'; c.async = true;
+  c.src = 'https://dpao6ma8eqkx0.cloudfront.net/js/o.min.js?v=0029';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(c, s);
+})();
